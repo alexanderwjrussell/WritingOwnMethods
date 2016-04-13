07355b11492d0420c9b60a71304987ebe5ae87b1
alexanderwjrussell <awjrussell@live.co.uk>
2016-04-13 18:14:40 +0100

    Chris Pines - Chapter 8 - Practice Code

---
+++ EnglishNumbers.rb
@@ -0,0 +1,16 @@
+def englishNumber number
+
+  if number < 0
+    return "Please enter a number which isn't negative"
+  end
+  if number == 0
+    return "zero"
+  end
+
+numString = ""
+
+onesPlace = [one, two, three, four, five, six, seven, eight, nine]
+tensPlace = [ten, twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety]
+teenPlace = [eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
+
+end
---
+++ SayMoo.rb
@@ -0,0 +1,7 @@
+def sayMoo numberOfMoos
+  puts "mooo..." * numberOfMoos
+  'yellow submarine'
+end
+
+x = sayMoo 4
+puts x
