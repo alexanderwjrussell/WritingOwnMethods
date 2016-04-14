c3d7f517d702f970d0c3ffe7e222c757fa011192
alexanderwjrussell <awjrussell@live.co.uk>
2016-04-14 22:03:48 +0100

    Writing the base code for the englishNumber practice code

--- EnglishNumbers.rb
+++ EnglishNumbers.rb
@@ -13,4 +13,31 @@ onesPlace = [one, two, three, four, five, six, seven, eight, nine]
 tensPlace = [ten, twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety]
 teenPlace = [eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
 
+left = number
+write = left/100
+
+left = left - write*100
+
+if write > 0
+  hundreds = englishNumber write
+  numString = numString hundreds + " hundred"
+
+  if left > 0
+    numString = numString + " "
+  end
 end
+
+write = left/10
+left = left - write*10
+
+if write > 0
+  if ((write == 0) and (left > 0))
+    numString = numString + teenPlace[left-1]
+    left = 0
+  else
+    numString = numString + tensPlace[write-1]
+  end
+
+end
+
+puts englishNumber(0)
